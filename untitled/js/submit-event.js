ba73ec497bd1ea39a00e8472f72115763649afa4
Clayton de Oliveira <clayton.barrozo@gmail.com>
2019-09-15 20:33:39 +0100

    Update submit-event.js

--- untitled/js/submit-event.js
+++ untitled/js/submit-event.js
@@ -1,38 +1,25 @@
-// This example displays how to get all contacts from a HubID and paginate through them using the 'offset' parameter.
-// The end result is an array containing all parsed contacts.
-
 var request = require("request");
-const returnedContacts = [];
-const API_KEY = 'a2565642-1a58-4a0e-a53a-4d83e4998ba3&userId=8813519';
-const count = 5;
 
+var options = { method: 'POST',
+  url: 'https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/testingapis@hubspot.com/',
+  qs: { hapikey: '2644317d-95d0-443f-8fc0-77d1f41df18b' },
+  headers:
+   {  'Content-Type': 'application/json' },
+  body:
+   { properties:
+      [ { property: 'firstname', value: 'HubSpot' },
+        { property: 'lastname', value: 'Test' },
+        { property: 'website', value: 'http://hubspot.com' },
+        { property: 'company', value: 'HubSpot' },
+        { property: 'phone', value: '555-122-2323' },
+        { property: 'address', value: '25 First Street' },
+        { property: 'city', value: 'Cambridge' },
+        { property: 'state', value: 'MA' },
+        { property: 'zip', value: '02139' } ] },
+  json: true };
 
-function getContacts(offset) {
-    if (typeof offset == 'undefined') {
-        offsetParam = null;
-    } else {
-        offsetParam = `vidOffset=${offset}`;
-    }
-    const hapikeyParam = `hapikey=${API_KEY}`;
-    const paramsString = `?count=${count}&${hapikeyParam}&${offsetParam}`;
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
 
-    const finalUrl = `https://api.hubapi.com/contacts/v1/lists/all/contacts/all${paramsString}`;
-    request(finalUrl, (error, response, body) => {
-        if (error) {
-            console.log('error', error);
-            throw new Error
-        }
-        const parsedBody = JSON.parse(body);
-        parsedBody.contacts.forEach(contact => {
-            returnedContacts.push(contact);
-        });
-        if (parsedBody['has-more']) {
-            getContacts(parsedBody['vid-offset'])
-        } else {
-            //print out all contacts
-            console.log(returnedContacts)
-        }
-    })
-}
-
-getContacts();
+  console.log(body);
+});
